7e6335a4c07574ae0b4905eaf870f80dbe56d408
rupamjain <rnj323@gmail.com>
2019-04-14 04:28:23 +0530

    modified

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express=require('express');
 const fs = require('fs');
 const app=express();
 
+
+//using stream
 app.get('/',(req,res)=>{
    
     const src = fs.createReadStream('./bible.txt');
     src.pipe(res);
 });
+
+
+//without stream
 app.get('/2',(req,res)=>{
      
     fs.readFile('./bible.txt', (err, data) => {
@@ -14,6 +19,7 @@ app.get('/2',(req,res)=>{
         res.end(data);       
       });
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
